f723382b3edf69caef200e1ac0a6c2c59025f0fb
me-dangnhatminh <dangnhatminh09032002@gmail.com>
2026-05-28 23:11:59 +0700

    fix: add missing Cognito authorization to API Gateway GET /workspaces

--- w7/terraform/api-gateway.tf
+++ w7/terraform/api-gateway.tf
@@ -18,7 +18,8 @@ resource "aws_api_gateway_method" "workspaces_get" {
   rest_api_id   = aws_api_gateway_rest_api.app.id
   resource_id   = aws_api_gateway_resource.workspaces.id
   http_method   = "GET"
-  authorization = "NONE"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito.id
 }
 
 resource "aws_api_gateway_method" "workspaces_post" {
